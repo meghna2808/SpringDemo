c7caf88a8a5a2f6eab7539f0f742358b824bc65b
meghnas2808 <meghnasingh2808@gmail.com>
2025-02-02 18:59:33 +0530

    using constructor injection to get age

--- src/main/java/org/example/Dev.java
+++ src/main/java/org/example/Dev.java
@@ -1,13 +1,20 @@
 package org.example;
 
 public class Dev {
-    Dev()
+    private int age;
+    public Dev()
     {
         System.out.println("dev created");
+    }
+    public Dev(int age)
+    {
+        this.age=age;
     }
     public void build()
     {
 
         System.out.println( "dev working" );
+        System.out.println(age);
+//        laptop.compile();
     }
 }
